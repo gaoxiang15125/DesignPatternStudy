c36aed20448c70dc75f42252c12a95a40aac286e
gaoxiang <630268696@qq.com>
2021-01-22 15:14:30 +0800

    使用阿里初始化工具创建项目 集成 Lombok \ Spring web

---
+++ src/main/java/github/gx/designstudy/DesignStudyApplication.java
@@ -0,0 +1,13 @@
+package github.gx.designstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignStudyApplication.class, args);
+    }
+
+}
